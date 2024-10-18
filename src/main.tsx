cb7eb8bcb25e8588fd3a73a52f44149a5041c656
nuuuri <sbfl125@gmail.com>
2024-10-19 02:09:18 +0900

    [feat] globalStyles 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { Global } from '@emotion/react';
+
 import App from './App.tsx';
-import './index.css';
+import { globalStyles } from './styles/globalStyles.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </StrictMode>
 );
