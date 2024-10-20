6daa610f7696e5f0919d0bee6ce40dff461ad026
nuuuri <sbfl125@gmail.com>
2024-10-21 00:14:41 +0900

    [feat] Button 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,163 @@
+import { CSSProperties, MouseEventHandler } from 'react';
+
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+type ButtonColors = 'default' | 'primary' | 'danger';
+type ButtonVariant = 'solid' | 'outlined' | 'dashed' | 'filled';
+
 interface ButtonProps {
-  children: string;
+  color?: ButtonColors;
+  variant?: ButtonVariant;
+  disabled?: boolean;
+  children?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  style?: CSSProperties;
 }
 
-export default function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+const defaultStyle = css`
+  padding: 4px 8px;
+  border-radius: 4px;
+  color: var(--ant-gray-11);
+  cursor: pointer;
+`;
+
+const buttonColors = {
+  default: 'gray',
+  primary: 'blue',
+  danger: 'red',
+};
+
+const Solid = styled.button<{ color: ButtonColors }>`
+  ${defaultStyle}
+
+  color: var(--ant-gray-1);
+
+  ${(props) => {
+    if (props.color === 'default') {
+      return css`
+        background: var(--ant-gray-12);
+        border: 1px solid var(--ant-gray-12);
+
+        :hover {
+          background: var(--ant-gray-8);
+        }
+        :active {
+          background: var(--ant-gray-12);
+        }
+      `;
+    }
+
+    return css`
+      background: var(--ant-${buttonColors[props.color]}-6);
+      border: 1px solid var(--ant-${buttonColors[props.color]}-6);
+
+      :hover {
+        background: var(--ant-${buttonColors[props.color]}-4);
+      }
+      :active {
+        background: var(--ant-${buttonColors[props.color]}-7);
+      }
+    `;
+  }}
+`;
+
+const Filled = styled.button<{ color: ButtonColors }>`
+  ${defaultStyle}
+
+  ${(props) => {
+    if (props.color === 'default') {
+      return css`
+        background: var(--ant-gray-3);
+        border: 1px solid var(--ant-gray-3);
+
+        :hover {
+          background: var(--ant-gray-4);
+        }
+        :active {
+          background: var(--ant-gray-5);
+        }
+      `;
+    }
+
+    return css`
+      background: var(--ant-${buttonColors[props.color]}-2);
+      border: 1px solid var(--ant-${buttonColors[props.color]}-2);
+
+      :hover {
+        background: var(--ant-${buttonColors[props.color]}-3);
+      }
+      :active {
+        background: var(--ant-${buttonColors[props.color]}-4);
+      }
+    `;
+  }}
+`;
+
+const Outlined = styled.button<{ color: ButtonColors }>`
+  ${defaultStyle}
+
+  background: var(--ant-gray-1);
+
+  ${(props) => {
+    if (props.color === 'default') {
+      return css`
+        border: 1px solid var(--ant-gray-5);
+
+        :hover {
+          border: 1px solid var(--ant-blue-5);
+          color: var(--ant-blue-5);
+        }
+        :active {
+          border: 1px solid var(--ant-blue-7);
+          color: var(--ant-blue-7);
+        }
+      `;
+    }
+
+    return css`
+      border: 1px solid var(--ant-${buttonColors[props.color]}-5);
+      color: var(--ant-${buttonColors[props.color]}-5);
+
+      :hover {
+        border: 1px solid var(--ant-${buttonColors[props.color]}-3);
+        color: var(--ant-${buttonColors[props.color]}-3);
+      }
+      :active {
+        border: 1px solid var(--ant-${buttonColors[props.color]}-7);
+        color: var(--ant-${buttonColors[props.color]}-7);
+      }
+    `;
+  }}
+`;
+
+const Dashed = styled(Outlined)`
+  &,
+  :hover,
+  :active {
+    border-style: dashed;
+  }
+`;
+
+export default function Button({
+  color = 'default',
+  variant = 'outlined',
+  disabled = false,
+  children,
+  onClick,
+  style,
+}: ButtonProps) {
+  const props = {
+    color: color,
+    disabled: disabled,
+    onClick: onClick,
+    style: style,
+  };
+
+  return {
+    solid: <Solid {...props}>{children}</Solid>,
+    outlined: <Outlined {...props}>{children}</Outlined>,
+    dashed: <Dashed {...props}>{children}</Dashed>,
+    filled: <Filled {...props}>{children}</Filled>,
+  }[variant];
 }
--- src/pages/button/index.tsx
+++ src/pages/button/index.tsx
@@ -1,11 +1,61 @@
+import styled from '@emotion/styled';
+
 import Button from '@components/Button';
+
+const Box = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
 
 export default function ButtonPage() {
   return (
     <div>
-      button 컴포넌트 설명 페이지
-      <br />
-      <Button>기본 버튼</Button>
+      <h3>default</h3>
+      <Box>
+        <Button color="default" variant="solid">
+          solid
+        </Button>
+        <Button color="default" variant="outlined">
+          outlined
+        </Button>
+        <Button color="default" variant="dashed">
+          dashed
+        </Button>
+        <Button color="default" variant="filled">
+          filled
+        </Button>
+      </Box>
+      <h3>primary</h3>
+      <Box>
+        <Button color="primary" variant="solid">
+          solid
+        </Button>
+        <Button color="primary" variant="outlined">
+          outlined
+        </Button>
+        <Button color="primary" variant="dashed">
+          dashed
+        </Button>
+        <Button color="primary" variant="filled">
+          filled
+        </Button>
+      </Box>
+      <h3>danger</h3>
+      <Box>
+        <Button color="danger" variant="solid">
+          solid
+        </Button>
+        <Button color="danger" variant="outlined">
+          outlined
+        </Button>
+        <Button color="danger" variant="dashed">
+          dashed
+        </Button>
+        <Button color="danger" variant="filled">
+          filled
+        </Button>
+      </Box>
     </div>
   );
 }
